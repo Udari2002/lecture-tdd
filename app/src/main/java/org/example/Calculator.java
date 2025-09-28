35094f32982bc3452906c85de1991da97249d9a4
Udari Moksha Perera <139832186+Udari2002@users.noreply.github.com>
2025-09-28 08:29:00 +0000

    make calcculator class with tests

---
+++ app/src/main/java/org/example/Calculator.java
@@ -0,0 +1,8 @@
+
+package org.example;
+
+public class Calculator {
+    public int addition(int a, int b) {
+        return a + b;
+    }
+}
---
+++ app/src/test/java/org/SampleTest.java
@@ -0,0 +1,14 @@
+package org;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+    @BeforeEach
+
+
+    @Test
+    public void printGreet() {
+        System.out.println("hello world");
+    }
+}
